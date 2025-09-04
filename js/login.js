aae1cbd8018809cb1ef9acc0f54f10f54c04dd20
dictetengs <dictetengs@gmail.com>
2025-09-04 13:34:11 +0200

    midtvejs commit

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 
   const modal = document.getElementById('loginModal');
-
+  const email = document.getElementById('email').value;
+  const pwd = document.getElementById('pwd').value;
+  
   function openModal() {
     modal.classList.add('open');
     document.body.style.overflow = 'hidden'; // stop scroll bagved
@@ -10,5 +12,26 @@
     document.body.style.overflow = ''; // tillad scroll igen
   }
 
-  
+const emails = [
+  "tbj@ucl.dk",
+  "test@haha.dk",
+  "joe@biden.com",
+  "freakyah@gmail.com",
+  "dicte@ersød.dk"
+
+]
+const koder = [
+  "hest",
+  "lortejavascript",
+  "dicteersødogsjov",
+  "test1234",
+  "jeghedderthomas"
+]
+
+function login(){
+if (emails.includes(email))
+{console.log ('email korrekt');
+  alert("du er nu logget ind");
+}
+}
 
